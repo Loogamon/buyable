cfd7f2ca29edbd190c8f114401f78e5558f38123
Zachary Tabikh <105983318+Loogamon@users.noreply.github.com>
2023-12-09 11:00:48 -0500

    Oops, I learned Linux doesn't work with \\ paths.

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ from flask_app.controllers import pages
 from flask_app.controllers import actions
 from flask_app.controllers import actions_user
 
-UPLOAD_FOLDER = "flask_app\\static\\img\\uploads"
+UPLOAD_FOLDER = "flask_app/static/img/uploads"
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 if __name__=="__main__":
